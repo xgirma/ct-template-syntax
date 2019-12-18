0a494db3a1c0a378e28924c77d45900cb61e0b67
Girma Nigusse <xgirma@gmail.com>
2019-12-17 23:03:13 -0500

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { ListItemComponent } from './list-item/list-item.component';
-import { SizerComponent } from './sizer/sizer.component';
 
 @NgModule({
-  declarations: [ AppComponent, ItemDetailComponent, ListItemComponent, SizerComponent ],
+  declarations: [ AppComponent, ItemDetailComponent, ListItemComponent ],
   imports: [ BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
